d015a68028287df1d9483010427082236390ed55
Mayur Ankush Gawade <mgawade@mobiquityinc.com>
2019-01-15 16:30:35 +0530

    OEPOC-1:Done the UI part of login page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  clickMessage = '';
+  loginForm: FormGroup;
+  constructor(private readonly formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+  onLogin() {
+    console.warn(this.loginForm.value);
+    this.clickMessage = 'Test Started Successfully';
+    this.loginForm.reset();
+  }
+
+}
